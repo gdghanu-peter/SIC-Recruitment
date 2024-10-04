f18efd98f85cb9dcbb9c3677808f9e44f1815f7d
dncmaduro <manhdungtran.vnuuet@gmail.com>
2024-10-04 12:34:17 +0700

    feat: banner

---
+++ src/components/header/item/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <nuxt-link :to="to">
+    <span class="font-bold text-sm text-black text-hover-primary">{{
+      label
+    }}</span>
+  </nuxt-link>
+</template>
+
+<script setup lang="ts">
+import type { HeaderItem } from '~/types/header'
+
+defineProps<HeaderItem>()
+</script>
---
+++ src/pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="w-screen">
+    <HomeBanner />
+  </div>
+</template>
