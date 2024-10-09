2c85499653ffcfbfcd225b0aabdbddf547ef60d0
Peter <peter@Peters-MacBook-Air.local>
2024-10-09 10:39:23 +0700

    revert  validate

--- src/components/apply/form/general/index.vue
+++ src/components/apply/form/general/index.vue
@@ -1,31 +1,13 @@
 <template>
   <div class="mt-8 pb-[100px] flex flex-col items-center">
     <span class="uppercase text-form-grow font-bold text-3xl">Phần 01</span>
-    <UForm
-      :state="state"
-      class="lg:w-[820px] w-[400px] flex flex-col lg:flex-row lg:flex-wrap"
-      @submit="handleSubmit"
-    >
-      <ApplyFormQuestion
-        v-for="question in generalQuestionData"
-        :key="question.question"
-        :image="question.image"
-        :input-type="question.inputType"
-        :placeholder="question.placeholder"
-        :question="question.question"
-        :required="question.required"
-        :name="question.name"
-        :basis="question.basis"
-        @change="handleChange"
-      />
+    <UForm :state="state" :validate="validate" class="lg:w-[820px] w-[400px] flex flex-col lg:flex-row lg:flex-wrap"
+      @submit="handleSubmit">
+      <ApplyFormQuestion v-for="question in generalQuestionData" :key="question.question" :image="question.image"
+        :input-type="question.inputType" :placeholder="question.placeholder" :question="question.question"
+        :required="question.required" :name="question.name" :basis="question.basis" @change="handleChange" />
 
-      <UButton
-        class="bg-form mx-auto uppercase mt-8"
-        type="submit"
-        size="lg"
-        label="Tiếp theo"
-        :disabled="loading"
-      />
+      <UButton class="bg-form mx-auto uppercase mt-8" type="submit" size="lg" label="Tiếp theo" :disabled="loading" />
     </UForm>
   </div>
 </template>
@@ -35,6 +17,7 @@ import { useForm } from '~/composables/useForm'
 import { generalQuestionData } from '~/mocks/general-form'
 import { useChoice } from '~/stores/choice'
 import type { GeneralState } from '~/types/apply/general-state'
+import type { FormError } from '#ui/types'
 
 const choiceStore = useChoice()
 const router = useRouter()
@@ -55,20 +38,45 @@ const state = reactive<GeneralState>({
   futurePlans: '',
   photoThoughts: ''
 })
-const errors = reactive<Record<keyof GeneralState, string>>(
-  {} as Record<keyof GeneralState, string>
-)
 
+const errorMessage = 'Bạn cần điền vào trường này'
+
+const validate = (state: GeneralState): FormError[] => {
+  const errors: FormError[] = []
+
+  if (!state.name) errors.push({ path: 'name', message: errorMessage })
+  if (!state.dob) errors.push({ path: 'dob', message: errorMessage })
+  if (!state.gender) errors.push({ path: 'gender', message: errorMessage })
+  if (!state.highSchool)
+    errors.push({ path: 'highSchool', message: errorMessage })
+  if (!state.major) errors.push({ path: 'major', message: errorMessage })
+  if (!state.phoneNumber)
+    errors.push({ path: 'phoneNumber', message: errorMessage })
+  if (!state.email) errors.push({ path: 'email', message: errorMessage })
+  if (!state.facebook) errors.push({ path: 'facebook', message: errorMessage })
+  if (!state.avatar) errors.push({ path: 'avatar', message: errorMessage })
+  if (!state.extracurriculars)
+    errors.push({ path: 'extracurriculars', message: errorMessage })
+  if (!state.strengthsWeaknesses)
+    errors.push({ path: 'strengthsWeaknesses', message: errorMessage })
+  if (!state.uniqueness)
+    errors.push({ path: 'uniqueness', message: errorMessage })
+  if (!state.futurePlans)
+    errors.push({ path: 'futurePlans', message: errorMessage })
+  if (!state.photoThoughts)
+    errors.push({ path: 'photoThoughts', message: errorMessage })
+
+  return errors
+}
 
 const { generalForm } = useForm()
 
 const loading = ref(false)
 
 const handleSubmit = async () => {
- 
-
   loading.value = true
   try {
+
     const res = await generalForm(state)
 
     const firstChoice = choiceStore.first
